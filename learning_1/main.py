d9d80150d382386eb2feba89354d19bab7e8ddeb
Brian Obot <brianobot9@gmail.com>
2023-01-22 17:21:47 +0100

    Refactor code

--- learning_1/main.py
+++ learning_1/main.py
@@ -1,41 +1,50 @@
 import sys
 import pygame
 
-# Neccessary to ensure modules are initialized properly
-pygame.init()
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-size = width, height = (520, 440)
-screen = pygame.display.set_mode(size, pygame.SCALED) # set the window size
-pygame.display.set_caption('Test Game') # set the window title
-pygame.mouse.set_visible(False) # set the cursor within the window to invisible
 
-clock = pygame.time.Clock()
+def main():
+    # Neccessary to ensure modules are initialized properly
+    # Initialise screen
+    pygame.init()
 
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill(BLACK)
+    size = width, height = (520, 440)
+    screen = pygame.display.set_mode(size, pygame.SCALED) # set the window size
+    pygame.display.set_caption('Test Game') # set the window title
+    pygame.mouse.set_visible(False) # set the cursor within the window to invisible
 
-screen.blit(background, (0, 0))
-pygame.display.flip()
+    clock = pygame.time.Clock()
 
-while True:
-    # Process player inputs
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            raise SystemExit
+    # Fill background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill(BLACK)
 
-    # Do logical updates here.
-    # ...
+    # Blit everything to the screen
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
 
-    screen.fill("black")  # Fill the display with a solid color
+    while True:
+        # Process player inputs
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
 
-    # Render the graphics here.
-    # ...
+        # Do logical updates here.
+        # ...
 
-    pygame.display.flip()  # Refresh on-screen display
-    clock.tick(60)         # wait until next frame (at 60 FPS)
-            
+        screen.fill("black")  # Fill the display with a solid color
+
+        # Render the graphics here.
+        # ...
+
+        pygame.display.flip()  # Refresh on-screen display
+        clock.tick(60)         # wait until next frame (at 60 FPS)
+                
+
+if __name__ == "__main__":
+    main()
